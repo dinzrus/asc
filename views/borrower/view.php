8b0d945475199543b739fdc6a999e8fbf6ca3a39
Russel Dinoy <dinoyrussel@gmail.com>
2016-08-15 17:11:53 +0800

    update for today

--- views/borrower/view.php
+++ views/borrower/view.php
@@ -5,32 +5,31 @@ use yii\widgets\DetailView;
 use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Borrower */
+/* @var $borrower app\models\Borrower */
 
-$this->title = $model->id;
+$this->title = $borrower->id;
 $this->params['breadcrumbs'][] = ['label' => 'Borrower', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="borrower-view">
-
     <div class="row">
         <div class="col-sm-8">
-            <h2><?= 'Borrower'.' '. Html::encode($this->title) ?></h2>
+            <h2><?= 'Borrower' . ' ' . Html::encode($this->title) ?></h2>
         </div>
         <div class="col-sm-4" style="margin-top: 15px">
-<?=             
-             Html::a('<i class="fa glyphicon glyphicon-hand-up"></i> ' . 'PDF', 
-                ['pdf', 'id' => $model->id],
-                [
-                    'class' => 'btn btn-danger',
-                    'target' => '_blank',
-                    'data-toggle' => 'tooltip',
-                    'title' => 'Will open the generated PDF file in a new window'
-                ]
-            )?>
-            <?= Html::a('Save As New', ['save-as-new', 'id' => $model->id], ['class' => 'btn btn-info']) ?>            
-            <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            <?=
+            Html::a('<i class="fa glyphicon glyphicon-hand-up"></i> ' . 'PDF', ['pdf', 'id' => $borrower->id], [
+                'class' => 'btn btn-danger',
+                'target' => '_blank',
+                'data-toggle' => 'tooltip',
+                'title' => 'Will open the generated PDF file in a new window'
+                    ]
+            )
+            ?>
+            <?= Html::a('Save As New', ['save-as-new', 'id' => $borrower->id], ['class' => 'btn btn-info']) ?>            
+            <?= Html::a('Update', ['update', 'id' => $borrower->id], ['class' => 'btn btn-primary']) ?>
+            <?=
+            Html::a('Delete', ['delete', 'id' => $borrower->id], [
                 'class' => 'btn btn-danger',
                 'data' => [
                     'confirm' => 'Are you sure you want to delete this item?',
@@ -40,59 +39,86 @@ $this->params['breadcrumbs'][] = $this->title;
             ?>
         </div>
     </div>
-
     <div class="row">
-<?php 
-    $gridColumn = [
-        ['attribute' => 'id', 'hidden' => true],
-        'profile_pic',
-        'first_name',
-        'last_name',
-        'middle_name',
-        'suffix',
-        'birthdate',
-        'age',
-        'birthplace',
-        [
-            'attribute' => 'addressProvince.province',
-            'label' => 'Address Province',
-        ],
-        [
-            'attribute' => 'addressCityMunicipality.municipality_city',
-            'label' => 'Address City Municipality',
-        ],
-        [
-            'attribute' => 'addressBarangay.barangay',
-            'label' => 'Address Barangay',
-        ],
-        'address_street_house_no',
-        'civil_status',
-        'contact_no',
-        'ci_date',
-        'canvass_date',
-        'tin_no',
-        'sss_no',
-        'ctc_no',
-        'license_no',
-        'spouse_name',
-        'spouse_occupation',
-        'spouse_age',
-        'spouse_birthdate',
-        'no_dependent',
-        'collaterals:ntext',
-        [
-            'attribute' => 'status0.status',
-            'label' => 'Status',
-        ],
-        'branch_id',
-        'attachment:ntext',
-        'relation_to_applicant',
-        'acount_type',
-    ];
-    echo DetailView::widget([
-        'model' => $model,
-        'attributes' => $gridColumn
-    ]); 
-?>
+        <div class="row">
+            <div class="col-md-3">
+                <center>
+                    <?= Html::img($borrower->profile_pic, ['width' => 200, 'class' => 'img-thumbnail']) ?>
+                </center>
+            </div>
+            <div class="col-md-4">
+                <?php
+                $gridColumn = [
+                    //['attribute' => 'id', 'hidden' => true],
+                    'first_name',
+                    'last_name',
+                    'middle_name',
+                    'suffix',
+                    'birthdate',
+                    'age',
+                    'birthplace',
+                    [
+                        'attribute' => 'addressProvince.province',
+                        'label' => 'Address Province',
+                    ],
+                    [
+                        'attribute' => 'addressCityMunicipality.municipality_city',
+                        'label' => 'Address City Municipality',
+                    ],
+                    [
+                        'attribute' => 'addressBarangay.barangay',
+                        'label' => 'Address Barangay',
+                    ],
+                    'address_street_house_no',
+                    'civil_status',
+                    'contact_no',
+                ];
+                echo DetailView::widget([
+                    'model' => $borrower,
+                    'attributes' => $gridColumn
+                ]);
+                ?>
+            </div>
+            <div class="col-md-4">
+                <?php
+                $gridColumn = [
+                    'tin_no',
+                    'sss_no',
+                    'ctc_no',
+                    'license_no',
+                    'spouse_name',
+                    'spouse_occupation',
+                    'spouse_age',
+                    'spouse_birthdate',
+                    'collaterals:ntext',
+                    'ci_date',
+                    'canvass_date',
+                    [
+                        'attribute' => 'status0.status',
+                        'label' => 'Status',
+                    ],
+                    'branch_id',
+                ];
+                echo DetailView::widget([
+                    'model' => $borrower,
+                    'attributes' => $gridColumn
+                ]);
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-8">
+                <?php
+                $gridColumn = [
+                    'no_dependent',
+                ];
+                echo DetailView::widget([
+                    'model' => $borrower,
+                    'attributes' => $gridColumn
+                ]);
+                ?>
+            </div>
+        </div>
     </div>
 </div>
