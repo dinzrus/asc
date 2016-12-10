8b69316ee7a5438661499c90e3e9bd196466ef2b
Russel Dinoy <dinoyrussel@gmail.com>
2016-12-10 17:30:32 +0800

    update today

--- views/site/borrowerscollection.php
+++ views/site/borrowerscollection.php
@@ -19,9 +19,77 @@ $this->params['breadcrumbs'][] = 'Borrowers Collection';
     }
 </style>
 <div class="box box-primary">
-    <div class="box-body">
-        <a href="#myModal" class="btn btn-success" data-toggle="modal"><i class="fa fa-file"></i> New</a>
+    <div class="box-header">
+        <a href="#myModal" class="btn btn-success btn-lg" data-toggle="modal"><i class="fa fa-bookmark"></i> Select Unit</a>
     </div>
+    <div class="box-body">
+        <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#breakdown" data-toggle="tab"><i class="fa fa-arrow-circle-o-right"></i> <strong>Collection Breakdown</strong></a></li>
+                <li><a href="#collection" data-toggle="tab"><i class="fa fa-arrow-circle-o-right"></i> <strong>Collection</strong></a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="breakdown">    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-condensed table-hover">
+                                <tr>
+                                    <td class="break-label text-center"><strong>NO.</strong></td>
+                                    <td class="break-label text-center"><strong>DENOMINATION</strong></td>
+                                    <td class="break-label text-center"><strong>TOTAL COUNT</strong></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>1000</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>500</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>200</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>100</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>50</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="break-text form form-control text-center"></td>
+                                    <td class="text-center break-text"><strong>20</strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"><strong><p class="pull-right break-label">TOTAL COINS</p></strong></td>
+                                    <td><input type="text" class="break-text form form-control text-right"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"><strong><p class="pull-right break-label">TOTAL COLLECTION</p></strong></td>
+                                    <td class="text-right break-text">15,000.00</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="collection"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <button value="submit" class="btn btn-primary btn-lg pull-right"><i class="fa fa-save"></i> Save</button>
+            </div>
+        </div>
+
+    </div>     
 </div>
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -45,64 +113,15 @@ $this->params['breadcrumbs'][] = 'Borrowers Collection';
                                 <option value="3">A3</option>
                             </select>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <table class="table table-bordered table-condensed table-hover">
-                                    <tr>
-                                        <td class="break-label text-center"><strong>NO.</strong></td>
-                                        <td class="break-label text-center"><strong>DENOMINATION</strong></td>
-                                        <td class="break-label text-center"><strong>TOTAL COUNT</strong></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>1000</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>500</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>200</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>100</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>50</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="break-text form form-control text-center"></td>
-                                        <td class="text-center break-text"><strong>20</strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2"><strong><p class="pull-right break-label">TOTAL COINS</p></strong></td>
-                                        <td><input type="text" class="break-text form form-control text-right"></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2"><strong><p class="pull-right break-label">TOTAL COLLECTION</p></strong></td>
-                                        <td class="text-right break-text">15,000.00</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <div class="row">
                         <div class="col-md-6">
-                            <?= Html::button('Save', ['class' => 'btn btn-success btn-block']) ?>
+                            <button value="submit" class = 'btn btn-success btn-block'><i class="fa fa-save"></i> Submit</button>
                         </div>
                         <div class="col-md-6">
-                            <?= Html::button('Cancel', ['class' => 'btn btn-danger btn-block']) ?>
+                            <button class = 'btn btn-success btn-danger btn-block'>Cancel</button>
                         </div>
                     </div>
                 </div>
@@ -113,7 +132,7 @@ $this->params['breadcrumbs'][] = 'Borrowers Collection';
 
 <?php $this->registerJs("
         $(window).load(function(){
-            $('#myModal').modal('show');
+            //$('#myModal').modal('show');
         });
         ", View::POS_END); ?>
 
